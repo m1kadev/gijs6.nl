fab3ed4dbf95884293c339cf1aaa776d1f85d63e
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-04-07 17:21:09 +0200

    Restyle blog and fix RSS 3

--- bps/blog/routes.py
+++ bps/blog/routes.py
@@ -19,21 +19,25 @@ def blog_index():
     for post in os.listdir(os.path.join(BASE_DIR, "posts")):
         with open(os.path.join(BASE_DIR, "posts", post)) as file:
             file_content = file.read().strip()
+
         extract = re.match(r"^---\n(.*?)\n---\n", file_content, re.DOTALL)
         post_data = yaml.safe_load(extract.group(1))
         post_data["url"] = "/blog/" + post.removesuffix(".md")
-        lines = subprocess.check_output(["git", "log", "--format=%H %ct", "--", f"posts/{post}"], text=True, cwd=BASE_DIR).strip().split("\n")
-        try:
-            post_data["date"] = datetime.fromtimestamp(int(lines[-1].split()[1])).strftime("%d-%m-%Y")
-        except:
-            post_data["date"] = "lol"
+
+        if not post_data.get("date"):
+            lines = subprocess.check_output(["git", "log", "--format=%ct", "--", f"posts/{post}"], text=True, cwd=BASE_DIR).strip().split("\n")
+            try:
+                post_data["date"] = datetime.fromtimestamp(int(lines[-1])).strftime("%d-%m-%Y")
+            except (ValueError, IndexError):
+                post_data["date"] = "Not yet published"
+    
         post_list.append(post_data)
     return render_template("index.html", post_list = post_list)
 
 
-@blog_bp.route("/<string:title>")
-def blog_post(title):
-    file_name = title + ".md"
+@blog_bp.route("/<string:slug>")
+def blog_post(slug):
+    file_name = slug + ".md"
     try:
         with open(os.path.join(BASE_DIR, "posts", file_name)) as file:
             file_content = file.read().strip()
@@ -41,17 +45,26 @@ def blog_post(title):
         return redirect(url_for("blog_bp.blog_index"))
 
     lines = subprocess.check_output(["git", "log", "--format=%H %ct", "--", f"posts/{file_name}"], text=True, cwd=BASE_DIR).strip().split("\n")
-
-    post_info = {
-        "latest_commit": {
-            "hash": lines[0].split()[0],
-            "datetime": datetime.fromtimestamp(int(lines[0].split()[1])).strftime("%d-%m-%Y at %H:%M:%S")
-        },
-        "first_commit": {
-            "hash": lines[-1].split()[0],
-            "datetime": datetime.fromtimestamp(int(lines[-1].split()[1])).strftime("%d-%m-%Y at %H:%M:%S")
+    try:
+        post_info = {
+            "latest_commit": {
+                "hash": lines[0].split()[0],
+                "datetime": datetime.fromtimestamp(int(lines[0].split()[1])).strftime("%d-%m-%Y at %H:%M:%S")
+            },
+            "first_commit": {
+                "hash": lines[-1].split()[0],
+                "datetime": datetime.fromtimestamp(int(lines[-1].split()[1])).strftime("%d-%m-%Y at %H:%M:%S")
+            }
         }
-    }
+    except (ValueError, IndexError):
+        post_info = {
+            "latest_commit": {
+                "datetime": "unknown"
+            },
+            "first_commit": {
+                "datetime": "unknown"
+            }
+        }
 
     extract = re.match(r"^---\n(.*?)\n---\n", file_content, re.DOTALL)
     post_data = yaml.safe_load(extract.group(1))
@@ -70,14 +83,10 @@ def generate_rss_feed():
     feed.title("Gijs6 - Blog")
     feed.link(href="http://gijs6.nl/blog", rel="self")
     feed.description("My own blog")
+    feed.author(name="Gijs ten Berg - Gijs6", email="gijs6@dupunkto.org")
+    feed.language("en")
 
     for post in os.listdir(os.path.join(BASE_DIR, "posts")):
-        try:
-            lines = subprocess.check_output(["git", "log", "--format=%H %ct", "--", f"posts/{post}"], text=True, cwd=BASE_DIR).strip().split("\n")
-            date = datetime.fromtimestamp(int(lines[-1].split()[1]), pytz.timezone('Europe/Amsterdam'))
-        except IndexError:
-            continue
-
         with open(os.path.join(BASE_DIR, "posts", post)) as file:
             file_content = file.read().strip()
         extract = re.match(r"^---\n(.*?)\n---\n", file_content, re.DOTALL)
@@ -88,6 +97,14 @@ def generate_rss_feed():
 
         file_content_clean = re.sub(r"^---\s*\n.*?\n---\s*\n", "", file_content, flags=re.DOTALL)
 
+        if not post_data.get("date"):
+            try:
+                lines = subprocess.check_output(["git", "log", "--format=%ct", "--", f"posts/{post}"], text=True, cwd=BASE_DIR).strip().split("\n")
+                date = datetime.fromtimestamp(int(lines[-1]), pytz.timezone("Europe/Amsterdam"))
+            except (ValueError, IndexError):
+                # Not published posts
+                continue
+
         html_content = commonmark.commonmark(file_content_clean)
 
         entry = feed.add_entry()
@@ -95,6 +112,7 @@ def generate_rss_feed():
         entry.link(href=f"http://gijs6.nl/blog/{slug}")
         entry.description(html_content)
         entry.pubDate(date)
+        entry.author(name="Gijs ten Berg - Gijs6", email="gijs6@dupunkto.org")
 
     return feed.rss_str()
 
